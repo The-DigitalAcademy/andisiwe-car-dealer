35bad31519cfc3722fc772745752e3cd7a45b38e
DA M1 61 <DAM161@DAs-MacBook-48.local>
2022-08-03 15:52:23 +0200

    added everything

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
 
 const About = () => {
   return (
     <div>
-         <Header name="ABOUT US" />
+      <Header name="ABOUT US" />
 
-         <div class="container">
-        <p className='text-black fs-5'>The Toyota Extended Service Plan is designed to give you a value-added product solution. <br></br> Quality Service. Genuine Parts. Advanced Driving Course. Finance Products. Accesories. Models: <br></br>Corolla, Fortuner, Yaris, Hilux, Rav 4, Rush, Etios, S'esfikile, Avanza, Aygo.
-</p>
-
-<p>Toyota Motor Corporation is a Japanese <br></br> multinational automotive manufacturer headquartered in Toyota City, Aichi, Japan. <br></br> It was founded by Kiichiro Toyoda and incorporated on August 28, 1937. Toyota is one of the largest automobile manufacturers in the world, producing about 10 million vehicles per year.</p>
+      <div class="container">
+        <p className="text-black fs-5">
+          The Toyota Extended Service Plan is designed to give you a value-added
+          product solution. <br></br> Quality Service. Genuine Parts. Advanced
+          Driving Course. Finance Products. Accesories. Models: <br></br>
+          Corolla, Fortuner, Yaris, Hilux, Rav 4, Rush, Etios, S'esfikile,
+          Avanza, Aygo.
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
